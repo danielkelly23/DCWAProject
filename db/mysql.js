c0ffd6fb49c3ea6ea1dff3c7c5a81f1c0445fe73
DANIEL KELLY <G00421651@atu.ie>
2025-01-03 00:50:31 +0000

    Establish mySQL Database

---
+++ db/mysql.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql2');
+
+//Method to allow our app to access our mySQL Database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'proj2024mysql',
+});
+
+//Console log if Database is connected
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL');
+});
+
+//used a query to make sure i have access to data
+/*
+connection.query('SELECT * FROM student', (err, results) => {
+    if (err) throw err;
+    console.log('Students:', results);
+  });
+*/
+
+module.exports = connection;
